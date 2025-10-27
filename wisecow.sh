c360d8922d2ccccd931555f22c421c6f8524eb5a
Sayindra Rathod <sahilrathodrk31@gmail.com>
2025-10-27 21:13:41 +0530

    Update wisecow.sh

--- wisecow.sh
+++ wisecow.sh
@@ -1,29 +1,22 @@
 #!/usr/bin/env bash
-
 SRVPORT=4499
 RSPFILE=response
-
 rm -f $RSPFILE
 mkfifo $RSPFILE
-
 get_api() {
 	read line
 	echo $line
 }
-
 handleRequest() {
     # 1) Process the request
 	get_api
 	mod=`fortune`
-
 cat <<EOF > $RSPFILE
 HTTP/1.1 200
-
-
+<h3>CI/CD Test Deployment</h3>
 <pre>`cowsay $mod`</pre>
 EOF
 }
-
 prerequisites() {
 	command -v cowsay >/dev/null 2>&1 &&
 	command -v fortune >/dev/null 2>&1 || 
@@ -32,15 +25,12 @@ prerequisites() {
 			exit 1
 		}
 }
-
 main() {
 	prerequisites
 	echo "Wisdom served on port=$SRVPORT..."
-
 	while [ 1 ]; do
 		cat $RSPFILE | nc -lN $SRVPORT | handleRequest
 		sleep 0.01
 	done
 }
-
 main
